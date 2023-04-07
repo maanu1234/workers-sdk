71ee52f54963b9070c34d309bed15efe4e8521cf
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-04-07 07:22:23 +0200

    [wrangler] teach wrangler to search docs
    fix: use correct key
    
    
    fix: maybe this?

--- packages/wrangler/src/docs/index.ts
+++ packages/wrangler/src/docs/index.ts
@@ -1,75 +1,19 @@
+import { fetch } from "undici";
 import { printWranglerBanner } from "..";
 import { readConfig } from "../config";
 import { logger } from "../logger";
 import * as metrics from "../metrics";
 import openInBrowser from "../open-in-browser";
-
 import type {
 	CommonYargsArgv,
 	StrictYargsOptionsToInterface,
 } from "../yargs-types";
 
-const argToUrlHash = {
-	d1: "d1",
-	docs: "docs",
-	init: "init",
-	generate: "generate",
-	dev: "dev",
-	publish: "publish",
-	delete: "delete",
-	"kv:namespace": "kvnamespace",
-	"kv:key": "kvkey",
-	"kv:bulk": "kvbulk",
-	"r2 bucket": "r2-bucket",
-	"r2 object": "r2-object",
-	secret: "secret",
-	"secret:bulk": "secretbulk",
-	tail: "tail",
-	pages: "pages",
-	login: "login",
-	logout: "logout",
-	whoami: "whoami",
-	types: "types",
-	deployments: "deployments",
-};
-
 export function docsOptions(yargs: CommonYargsArgv) {
 	return yargs.positional("command", {
 		describe: "Enter the wrangler command you want to know more about",
 		type: "string",
-		// requiresArg: true,
-		choices: [
-			"docs",
-			"init",
-			"generate",
-			"dev",
-			"publish",
-			"delete",
-			"tail",
-			"secret",
-			"secret:bulk",
-			"kv:namespace",
-			"kv:key",
-			"kv:bulk",
-			"pages",
-			// "queues", //TODO: Undocumented
-			"r2 object",
-			"r2 bucket",
-			// "dispatch-namespace", // TODO: Undocumented - Workers for Platforms
-			"d1",
-			// "pubsub", //TODO: Undocumented
-			"login",
-			"logout",
-			"whoami",
-			"types",
-			"deployments",
-			"api",
-		],
 	});
-}
-
-function isValidParam(k: string): k is keyof typeof argToUrlHash {
-	return k in argToUrlHash;
 }
 
 export async function docsHandler(
@@ -78,12 +22,28 @@ export async function docsHandler(
 	let urlToOpen =
 		"https://developers.cloudflare.com/workers/wrangler/commands/";
 
-	if (args.command === "api") {
-		//if api, take them to the API docs
-		urlToOpen = "https://developers.cloudflare.com/workers/wrangler/api/";
-	} else if (args.command && isValidParam(args.command)) {
-		//otherwise, they get the wrangler commands page
-		urlToOpen += `#${argToUrlHash[args.command]}`;
+	if (args.command) {
+		const id = "8MU1G3QO9P";
+		const index = "developers-cloudflare2";
+		const key = "045e8dbec8c137a52f0f56e196d7abe0";
+		const searchResp = await fetch(
+			`https://${id}-dsn.algolia.net/1/indexes/${index}/query`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					params: `query=${args.command}&hitsPerPage=1&getRankingInfo=0`,
+				}),
+				headers: {
+					"X-Algolia-API-Key": key,
+					"X-Algolia-Application-Id": id,
+				},
+			}
+		);
+		const searchData = (await searchResp.json()) as { hits: { url: string }[] };
+		logger.debug("searchData: ", searchData);
+		if (searchData.hits[0]) {
+			urlToOpen = searchData.hits[0].url;
+		}
 	}
 
 	await printWranglerBanner();
